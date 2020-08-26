439bbf37d255e147d781e4997d4436eb0acf5a7b
gavar <max.stankevich.me@gmail.com>
2020-08-27 00:24:22 +0300

    test: assertion utils for Xunit

---
+++ src/Escher.Xunit/Asserts.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Escher.Xunit
+{
+    public abstract class Asserts : global::Xunit.Assert
+    {
+        private static readonly JsonSerializerOptions SERIALIZER_OPTIONS = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        };
+
+        public static void FileLines(string path, string[] expected)
+        {
+            var expect = string.Join(Environment.NewLine, expected) + Environment.NewLine;
+            Equal(expect, File.ReadAllText(path), ignoreLineEndingDifferences: true);
+        }
+
+        public static void FileJson<T>(string path, T expected)
+        {
+            var actual = NormalizeJsonText<T>(File.ReadAllText(path));
+            var expect = NormalizeJsonData(expected);
+            Equal(expect, actual, false, true, true);
+        }
+
+        private static string NormalizeJsonText<T>(string json)
+        {
+            var data = JsonSerializer.Deserialize<T>(json);
+            return NormalizeJsonData(data);
+        }
+
+        private static string NormalizeJsonData<T>(T data)
+        {
+            return JsonSerializer.Serialize(data, SERIALIZER_OPTIONS);
+        }
+    }
+}
