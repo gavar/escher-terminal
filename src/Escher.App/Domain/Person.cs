46e0d08103a27a9fb06f42d2e80debedb6fe18f6
gavar <max.stankevich.me@gmail.com>
2020-08-27 05:58:52 +0300

    fix: person serialization configuration

--- src/Escher.App/Domain/Person.cs
+++ src/Escher.App/Domain/Person.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using Escher.Data;
+using Escher.Data.Engine.Flat;
 using FlatFile.Delimited.Attributes;
 
 namespace Escher.App.Domain
 {
-    [DelimitedFile]
+    [DelimitedFile(Delimiter = ", ", HasHeader = true)]
     public class Person : Entity
     {
-        [DelimitedField(2)]
+        [DelimitedField(2, Name = nameof(FirstName))]
         public string FirstName { get; set; }
 
-        [DelimitedField(3)]
+        [DelimitedField(3, Name = nameof(LastName))]
         public string LastName { get; set; }
 
-        [DelimitedField(4)]
+        [DelimitedField(4, Name = nameof(BirthDate), Converter = typeof(ShortDateFormat))]
         public DateTime BirthDate { get; set; }
 
-        [DelimitedField(5)]
+        [DelimitedField(5, Name = nameof(Authorized))]
         public bool? Authorized { get; set; }
 
-        [DelimitedField(6)]
+        [DelimitedField(6, Name = nameof(Spouse))]
         public Person Spouse { get; set; }
 
         public bool IsAuthorized => Authorized.GetValueOrDefault(false);
